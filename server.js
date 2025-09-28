870f9d0a03f7175b81fe57ecd9192e3e9d254108
kelicodes <kelidevske@gmail.com>
2025-09-28 16:41:08 +0300

    winter

--- server.js
+++ server.js
@@ -14,15 +14,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// --- Allowed origins for CORS ---
+
+
+// --- Express CORS middleware ---
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5000",
-  "https://darasa-six.vercel.app/"
+  "https://darasa-six.vercel.app"
 ];
 
-// --- Express CORS middleware ---
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -32,9 +33,11 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true, // important for cookies/auth headers
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   })
 );
+
 
 // --- Connect to DB ---
 Db();
